61557fa5a4213abeb9564ebaf680fdd6ca13d231
Alesia-V175 <vaivasilisa@gmail.com>
2022-08-10 14:40:31 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const string = str;
+    const brackets = bracketsConfig;
+    let arr = [];
+    let obj = {};
+
+    for (let i = 0; i < brackets.length; i++) {
+        let key = brackets[i][1];
+        let value = brackets[i][0];
+        arr.push(value);
+        obj[key] = value;
+    }
+
+   let result = [];
+   for (let i = 0; i < string.length; i++) {
+       let currentValue = string[i];
+       let endValue = result[result.length - 1];
+       if (currentValue === endValue && currentValue === obj[currentValue]) {
+           result.pop();
+       } else if (arr.includes(currentValue)) {
+           result.push(currentValue);
+       } else {
+           if (result.length === 0) {
+               return false;
+           }
+            if (obj[currentValue] === endValue) {
+                result.pop();
+            }
+       }
+   }
+   if (result.length === 0) {
+       return true;
+   } else {
+       return false;
+   }
 }
